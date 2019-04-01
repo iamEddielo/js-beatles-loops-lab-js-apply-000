6d9b9856de025f1f7c44d0459166220cab479fb3
Eddie Lopez <iameddielopez@gmail.com>
2019-04-01 22:43:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,4 +7,7 @@ function theBeatlesPlay(musicians, instruments) {
 
 function johnLennonFacts() {
   var array = [facts] } 
-  const facts = []
+  const facts = ["He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"]; 
